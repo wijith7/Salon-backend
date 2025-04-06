faf62df4e390bf8ee30de8a45f707929cdcface7
wijith <wijith7@gmail.com>
2025-04-06 17:30:49 +0930

    Add Controller Class for SalonService

---
+++ microservices/salon-service/src/main/java/com/wijith/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.wijith.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping
+    public  String HomeControllerHandler(){
+        return "salon microservice for salon booking system";
+    }
+}
