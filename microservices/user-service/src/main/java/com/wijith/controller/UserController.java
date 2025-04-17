1ea6a5b62ccd62975cd16072efac880dae921ef3
wijith <wijith7@gmail.com>
2025-04-18 08:57:03 +0930

    Change annotation

--- microservices/user-service/src/main/java/com/wijith/controller/UserController.java
+++ microservices/user-service/src/main/java/com/wijith/controller/UserController.java
@@ -5,7 +5,6 @@ import com.wijith.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,22 +21,22 @@ public class UserController {
     }
 
     @GetMapping("/api/users")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public List <User> getUser(){
        return userService.getAllUsers();
     }
     @GetMapping("/api/users/{userId}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public User getUserByID(@PathVariable("userId") Long id) throws Exception {
             return userService.getUserById(id);
     }
     @PutMapping("/api/users/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public User updateUser (@RequestBody User user, @PathVariable Long id) throws Exception {
         return userService.updateUser(id,user);
     }
     @DeleteMapping("/api/users/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.OK)
     public String deleteUserById(@PathVariable Long id ) throws Exception{
         userService.deleteUser(id);
         return "User deleted";
